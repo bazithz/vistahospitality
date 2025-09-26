938bcfaea2f7051a8ca72fac857b26782f44e1dd
Bazithz <bazisworkspace@gmail.com>
2025-09-26 16:39:43 +0530

    gallery, menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -881,3 +881,47 @@ document.addEventListener('DOMContentLoaded', function() {
       card.style.animationDelay = `${index * 0.2}s`;
   });
 });
+
+
+
+
+
+$(window).on('load', function () {
+
+        /*-------------------------------------
+         jQuery for Isotope initialization
+         -------------------------------------*/
+        var $container = $('#inner-isotope');
+
+        if ($container.length > 0) {
+
+            // Isotope initialization
+            var $isotope = $container.find('.featuredContainer').isotope({
+                filter: '*',
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+
+            // Isotope filter
+            $container.find('.isotop-classes-tab').on('click', 'a', function () {
+
+                var $this = $(this);
+                $this.parent('.isotop-classes-tab').find('a').removeClass('current');
+                $this.addClass('current');
+                var selector = $this.attr('data-filter');
+                $isotope.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    }
+                });
+                return false;
+
+            });
+        }
+    });// end window load function
